8b5b90ddc59b0b485622af2ee49fd711cfa5e3b4
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-03-03 11:07:35 +0530

    feat: initial setup with RN version 0.68

---
+++ types/index.ts
@@ -0,0 +1,17 @@
+type LocalAttachmentType = Record<string, unknown>;
+type LocalChannelType = Record<string, unknown>;
+type LocalCommandType = string;
+type LocalEventType = Record<string, unknown>;
+type LocalMessageType = Record<string, unknown>;
+type LocalReactionType = Record<string, unknown>;
+type LocalUserType = Record<string, unknown>;
+
+export type StreamChatGenerics = {
+  attachmentType: LocalAttachmentType;
+  channelType: LocalChannelType;
+  commandType: LocalCommandType;
+  eventType: LocalEventType;
+  messageType: LocalMessageType;
+  reactionType: LocalReactionType;
+  userType: LocalUserType;
+};
